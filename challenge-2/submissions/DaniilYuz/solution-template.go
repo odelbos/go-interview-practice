4177e0040ca59920006693f3b1b422c5863ed99a
DaniilYuz <georgiferstor@gmail.com>
2025-12-06 18:38:49 +0100

    Add solution for Challenge 2 (#843)
    Co-authored-by: go-interview-practice-bot[bot] <230190823+go-interview-practice-bot[bot]@users.noreply.github.com>

---
+++ challenge-2/submissions/DaniilYuz/solution-template.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	res := []byte(s)
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+	    res[i], res[j] = res[j], res[i] 
+	}
+	return string(res)
+}
